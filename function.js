baa9446beb2f3acb9db7013c16fe1d0c87c5a8f8
Tazida <tazidarahman5@gmail.com>
2022-08-07 22:23:15 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,14 @@
+// function declarartion
+function startFan() {
+  console.log("Stand up");
+  console.log("walk towards the switch");
+  console.log("press the switch");
+}
+
+// call the fuunction
+startFan();
+console.log("walking up in the morning");
+startFan();
+console.log("Eating lunch");
+console.log("watching js tutorial");
+startFan();
---
+++ parameter.js
@@ -0,0 +1,29 @@
+// function bringSingara(money) {
+//   console.log("mama singara den");
+//   console.log(money);
+// }
+
+// // call the function
+// bringSingara(100);
+
+function bringSingara(money) {
+  console.log("eto taka dichen:", money);
+  console.log("ai nen singara");
+}
+// bringSingara(100);
+
+// or
+// var taka = 100;
+// bringSingara(taka);
+
+function add(num1, num2) {
+  console.log("going to add:", num1, num2);
+}
+// add(123, 98);
+
+function sum(a, b, c, d, e) {
+  console.log(a, b, c, d, e);
+  var sum = a + b + c + d + e;
+  console.log(sum);
+}
+sum(15, 98, 56, 5, 9);
